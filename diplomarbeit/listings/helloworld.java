ac5476bf205f79fb3dc23aa8becafbb9cdd7da6e
Raphael Fleischlin <git@raefftec.ch>
2013-03-26 21:30:11 +0100

    vorlage diplomarbeit

---
+++ diplomarbeit/listings/helloworld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+	/**
+	 * Complex Hello World Example
+	 */
+	public static void main(String [] args) {
+		String a = "e";
+		String b = "l";
+		String c = "w";
+		String d = "o";
+		String e = "H";
+		String f = "r";
+		String g = "d";
+		String final1 = e + a + b + b + d;
+		String final2 = c + d + f + b + g;
+		String low = final1.toLowerCase();
+		// print it out!
+		System.out.println(low + " " + final2);
+	}
+}
